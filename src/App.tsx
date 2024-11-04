a77e94463ae3896d82ac210c2910a1f76ef80084
jglee96 <zakklee96@gmail.com>
2024-11-04 13:55:31 +0900

    feat: file input 추가
    - file input 추가
    - css variable theme 추가
    - index.html 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 export default function App() {
-  return <></>;
+  return (
+    <div className="grid w-full max-w-sm items-center gap-1.5">
+      <Label htmlFor="picture">Picture</Label>
+      <Input id="picture" type="file" />
+    </div>
+  );
 }
